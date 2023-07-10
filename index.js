9c11d08dcfb53ddfa51a2226183b2848e0abbd8c
Macraykorir <108871589+macray-korir@users.noreply.github.com>
2023-07-10 14:09:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,18 +18,9 @@ function isPalindrome(word) {
     end--;
   }
 
-  return true; // All characters matched, it's a palindrome
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // Add your own custom tests here
   console.log("Expecting: true");
